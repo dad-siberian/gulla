6c104435cf1e7115b266d93a925aeef66dbe1098
dad-siberian <dad.siberian@gmail.com>
2022-06-24 13:40:49 +0300

    Add links to books txt files

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import logging.config
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -12,18 +13,26 @@ logger = logging.getLogger(__file__)
 def render_website():
     with open('books.json', 'r', encoding='utf-8') as file:
         books = json.load(file)
+
+    book_files = os.listdir('books')
+    book_paths = list(map(lambda name: os.path.join('books', name), book_files))
+    book_paths.sort()
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    rendered_page = template.render(books=books)
+    rendered_page = template.render(
+        books=zip(books, book_paths)
+    )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
 def main():
     logging.config.dictConfig(LOGGING_CONFIG)
+    render_website()
     server = Server()
     server.watch('template.html', render_website)
     server.serve(root='.')
