b9e272fd34b067e40f10dff3aaddc62816631823
dad-siberian <dad.siberian@gmail.com>
2022-06-24 11:49:56 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,16 +1,15 @@
-import logging.config
 import json
+import logging.config
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 from log_config import LOGGING_CONFIG
 
 logger = logging.getLogger(__file__)
 
 
-def main():
-    logging.config.dictConfig(LOGGING_CONFIG)
-
+def render_website():
     with open('books.json', 'r', encoding='utf-8') as file:
         books = json.load(file)
     env = Environment(
@@ -21,6 +20,13 @@ def main():
     rendered_page = template.render(books=books)
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
+
+
+def main():
+    logging.config.dictConfig(LOGGING_CONFIG)
+    server = Server()
+    server.watch('template.html', render_website)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
