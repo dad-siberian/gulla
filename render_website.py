0abf7d4f854a10328c214ad6864d8f29dda5651e
dad-siberian <dad.siberian@gmail.com>
2022-07-05 09:04:38 +0300

    added pagination

--- render_website.py
+++ render_website.py
@@ -1,6 +1,7 @@
 import json
 import logging.config
 import os
+from math import ceil
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -14,7 +15,8 @@ logger = logging.getLogger(__file__)
 def render_website():
     with open('books.json', 'r', encoding='utf-8') as file:
         books = json.load(file)
-
+    books_on_page = 10
+    page_quantity = ceil(len(books) / books_on_page)
     folder_path = os.path.join('.', 'pages')
     os.makedirs(folder_path, exist_ok=True)
 
@@ -22,9 +24,14 @@ def render_website():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    for page_number, some_books in enumerate(chunked(books, 10)):
+
+    for page_number, some_books in enumerate(chunked(books, books_on_page), start=1):
         template = env.get_template('template.html')
-        rendered_page = template.render(books=some_books)
+        rendered_page = template.render(
+            books=some_books,
+            page_number=page_number,
+            page_quantity=page_quantity
+        )
         filepath = os.path.join(folder_path, f'index{page_number}.html')
         with open(filepath, 'w', encoding="utf8") as file:
             file.write(rendered_page)
@@ -35,7 +42,7 @@ def main():
     render_website()
     server = Server()
     server.watch('template.html', render_website)
-    server.serve(root='.')
+    server.serve(root='pages', default_filename='index1.html')
 
 
 if __name__ == '__main__':
